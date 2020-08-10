tom-seewald/openscap

commit ff9bab652d035299abcb768b1f9c5b1899437a61

Add initial FreeBSD support for process probe.

src/OVAL/probes/unix/process_probe.c
@@ -58,6 +58,20 @@
 #include "process_probe.h"
 #include "oscap_helpers.h"
 
+#if defined(OS_FREEBSD)
+#include <kvm.h>
+#include <sys/param.h>
+#include <sys/sysctl.h>
+#include <sys/user.h>
+#include <limits.h>
+#include <paths.h>
+
+#define SEC_PER_YR  31557600
+#define SEC_PER_DAY 86400
+#define SEC_PER_HR  3600
+#define SEC_PER_MIN 60
+#endif
+
 static oval_schema_version_t over;
 
 /* Convenience structure for the results being reported */
@@ -485,6 +499,164 @@ int process_probe_main(probe_ctx *ctx, void *arg)
 	return 0;
 }
 
+#elif defined(OS_FREEBSD)
+static char *convert_time(time_t t, char *tbuf, int tb_size)
+{
+	int days = t / SEC_PER_DAY;
+	int hrs = (t - days*SEC_PER_DAY) / SEC_PER_HR;
+	int min = (t - days*SEC_PER_DAY - hrs*SEC_PER_HR) / SEC_PER_MIN;
+	int sec = (t - days*SEC_PER_DAY - hrs*SEC_PER_HR - min*SEC_PER_MIN);
+
+	if (days)
+		snprintf(tbuf, tb_size, "%u-%02u:%02u:%02u", days, hrs, min, sec);
+	else
+		snprintf(tbuf, tb_size,	"%02u:%02u:%02u", hrs, min, sec);
+
+	return tbuf;
+}
+
+static int read_process(SEXP_t *cmd_ent, probe_ctx *ctx)
+{
+	struct kinfo_proc *proclist, *proc;
+	struct result_info r;
+	char buf[LINE_MAX];
+	int i, count;
+	int err = 1;
+	kvm_t *kd;
+
+	kd = kvm_openfiles(NULL, _PATH_DEVNULL, NULL, O_RDONLY, buf);
+
+	if (!kd)
+		return err;
+
+	proclist = kvm_getprocs(kd, KERN_PROC_PROC, 0, &count);
+
+	if (!proclist)
+		return err;
+
+	proc = proclist;
+
+	for (i = 0; i < count; i++, proc++) {
+		// Skip kthreads
+		if (proc->ki_ppid == 0)
+			continue;
+
+		SEXP_t *cmd_sexp;
+		char **argbuf = NULL;
+		argbuf = kvm_getargv(kd, proc, LINE_MAX);
+
+		if (!argbuf) {
+			r.command = "";
+		}
+		else {
+			char arg_dest[LINE_MAX] = {0};
+			int j;
+
+			for (j = 0; argbuf[j] != NULL; j++) {
+				strncat(arg_dest, argbuf[j], strlen(argbuf[j]));
+				strncat(arg_dest, " ", 1);
+			}
+
+			arg_dest[strlen(arg_dest)-1] = '\0';
+			r.command = arg_dest;
+		}
+
+		dI("Have command: %s", r.command);
+		cmd_sexp = SEXP_string_newf("%s", r.command);
+
+		if (probe_entobj_cmp(cmd_ent, cmd_sexp) == OVAL_RESULT_TRUE) {
+
+			r.pid = proc->ki_pid;
+			r.ppid = proc->ki_ppid;
+			r.priority = proc->ki_pri.pri_level;
+			r.ruid = proc->ki_ruid;
+			r.user_id = proc->ki_uid;
+
+			const char *fmt;
+			char tty_buf[64];
+			char exec_buf[64];
+			char start_buf[64];
+
+			time_t start_time = proc->ki_start.tv_sec;
+			struct timeval current_time;
+
+			gettimeofday(&current_time, NULL);
+			time_t exec_time = current_time.tv_sec - proc->ki_start.tv_sec;
+			r.exec_time = convert_time(exec_time, exec_buf, sizeof(exec_buf));
+
+			time_t curr_year = current_time.tv_sec / SEC_PER_YR;
+			time_t curr_day = current_time.tv_sec / SEC_PER_DAY;
+			time_t start_year = start_time / SEC_PER_YR;
+			time_t start_day = start_time / SEC_PER_DAY;
+
+			if (curr_year != start_year || curr_day != start_day)
+				fmt = "%b_%d";
+			else
+				fmt = "%H:%M:%S";
+
+			struct tm *loc_time = localtime(&start_time);
+			strftime(start_buf, sizeof(start_buf), fmt, loc_time);
+
+			r.start_time = start_buf;
+
+			switch(proc->ki_tdev) {
+				case NODEV:
+					r.tty = "?";
+					break;
+				default:
+					snprintf(tty_buf, sizeof(tty_buf), "TTY %ld", proc->ki_tdev);
+					r.tty = tty_buf; 
+					break;
+			}
+
+			switch (proc->ki_pri.pri_class) {
+				case SCHED_OTHER:
+					r.scheduling_class = "TS";
+					break;
+				case SCHED_FIFO:
+					r.scheduling_class = "FF";
+					break;
+				case SCHED_RR:
+					r.scheduling_class = "RR";
+					break;
+				default:
+					r.scheduling_class = "?";
+					break;
+			}
+
+			report_finding(&r, ctx);
+		}
+		SEXP_free(cmd_sexp);
+	}
+
+	if ( kvm_close(kd) ) {
+		return err;
+	
+	}
+	err = 0;
+
+	return err;
+}
+
+int process_probe_main(probe_ctx *ctx, void *arg)
+{
+	SEXP_t *ent;
+
+	ent = probe_obj_getent(probe_ctx_getobject(ctx), "command", 1);
+
+	if (ent == NULL) {
+		return PROBE_ENOVAL;
+	}
+
+	if (read_process(ent, ctx)) {
+		SEXP_free(ent);
+		return PROBE_EACCESS;
+	}
+
+	SEXP_free(ent);
+
+	return 0;
+}
 #else
 int process_probe_main(probe_ctx *ctx, void *arg)
 {

commit 89702196284aba97d2069e9ee8d566708ab26223

Use unsigned ints in convert_time() on FreeBSD.
Match snprintf()'s format specifiers.

Fixes: ff9bab652 ("Add initial FreeBSD support for process probe.")

src/OVAL/probes/unix/process_probe.c
@@ -502,10 +502,10 @@ int process_probe_main(probe_ctx *ctx, void *arg)
 #elif defined(OS_FREEBSD)
 static char *convert_time(time_t t, char *tbuf, int tb_size)
 {
-	int days = t / SEC_PER_DAY;
-	int hrs = (t - days*SEC_PER_DAY) / SEC_PER_HR;
-	int min = (t - days*SEC_PER_DAY - hrs*SEC_PER_HR) / SEC_PER_MIN;
-	int sec = (t - days*SEC_PER_DAY - hrs*SEC_PER_HR - min*SEC_PER_MIN);
+	unsigned int days = t / SEC_PER_DAY;
+	unsigned int hrs = (t - days*SEC_PER_DAY) / SEC_PER_HR;
+	unsigned int min = (t - days*SEC_PER_DAY - hrs*SEC_PER_HR) / SEC_PER_MIN;
+	unsigned int sec = (t - days*SEC_PER_DAY - hrs*SEC_PER_HR - min*SEC_PER_MIN);
 
 	if (days)
 		snprintf(tbuf, tb_size, "%u-%02u:%02u:%02u", days, hrs, min, sec);